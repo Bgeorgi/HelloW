d46f0008d74613270384e59bf94e507962c7869c
Bgeorgi <43473918+Bgeorgi@users.noreply.github.com>
2018-10-05 20:20:34 +0200

    Create labda.c

---
+++ labda.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+
+void kiir (int a, int b)
+{
+system ("clear");
+
+while (b--)
+printf ("\n");
+while (a--)
+printf (" ");
+printf ("X\n");
+usleep (500000);
+}
+
+int
+main ()
+{
+int max = 80;	//maxX
+int may = 23;	//maxY
+int x = 0;
+int y = 0;
+int modx = -1;
+int mody = -1;
+int f = 6;
+for (;;)
+{
+modx = x >= max - 1 ? modx * (-1) : modx;
+mody = y >= may - 1 ? mody * (-1) : mody;
+modx = x <= 1 ? modx * (-1) : modx;
+mody = y <= 1 ? mody * (-1) : mody;
+while ((max - (x + modx)) && (may - (y + mody)) && (y + mody)
+&& (x + modx))
+{
+x += modx;
+y += mody;
+kiir (x, y);
+}
+}
+return 0;
+}
